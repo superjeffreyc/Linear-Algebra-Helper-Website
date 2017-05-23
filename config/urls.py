6455c1c420706f131ddad4e2b13669d0d0afc100
Jeffrey Chan <jchan63@binghamton.edu>
2017-05-23 20:19:22 +0000

    Added basic site for Linear Algebra Helper app

---
+++ config/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+import src.views
+
+# Examples:
+# url(r'^$', 'gettingstarted.views.home', name='home'),
+# url(r'^blog/', include('blog.urls')),
+
+urlpatterns = [
+    url(r'^$', src.views.index, name='index'),
+    url(r'^privacy', src.views.privacy, name='privacy'),
+    #url(r'^admin/', include(admin.site.urls)),
+]
